6bf9a253294c0a4b18d538992c5c310f4dd6eff2
Lyudmil Anastasov <75137222+Anastasov-L@users.noreply.github.com>
2025-06-09 11:33:11 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,15 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
