b6f907abd7b028a97d739b43cf4b89ce617bffbc
Lyudmil Anastasov <75137222+Anastasov-L@users.noreply.github.com>
2025-06-09 12:46:38 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,25 @@ function App() {
     <>
       <Box w = "100vw" bg = "#0F1115">
         <SimpleGrid columns = {{base:1,sm:1,md:2,lg: 2}} rowGap = "25px" columnGap = "25px" w = "calc(100vw - 48px)">
-          <GridItem colSpan="1">
+          <GridItem colSpan= 1>
             <Box h = "18vh" borderRadius = "20px">
                <Text>Header</Text>
             </Box>
           </GridItem>
           
-           <GridItem bg = "#1C1F26" borderRadius = "20px" colSpan="1">
+           <GridItem bg = "#1C1F26" borderRadius = "20px" colSpan= 1>
              <Box h = "40vh">
                
              </Box>
            </GridItem>
   
-           <GridItem bg = "#1C1F26" borderRadius = "20px" colSpan="1">
+           <GridItem bg = "#1C1F26" borderRadius = "20px" colSpan= 1>
              <Box h = "40vh">
                
              </Box>
            </GridItem>
   
-           <GridItem bg = "#1C1F26" borderRadius = "20px" colSpan="1">
+           <GridItem bg = "#1C1F26" borderRadius = "20px" colSpan= 1>
              <Box h = "40vh">
                
              </Box>
