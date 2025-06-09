02d1ca3da3f60d4db6ffec41660db1eac7bb7060
Lyudmil Anastasov <75137222+Anastasov-L@users.noreply.github.com>
2025-06-09 13:16:02 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,26 @@ function App() {
   return (
     <>
       <Box w = "100vw" bg = "#0F1115">
-        <SimpleGrid columns = {{base:1,sm:1,md:2,lg: 2}} rowGap = "25px" columnGap = "25px" w = "calc(100vw - 48px)">
+        <SimpleGrid columns = {{base:1,sm:1,md:2,lg: 2,xl: 2}} rowGap = "25px" columnGap = "25px" w = "calc(100vw - 48px)">
           <GridItem >
-            <Box h = "18vh" borderRadius = "20px">
+            <Box h = "18vh" borderRadius = "20px" minW = "360px">
                <Text>Header</Text>
             </Box>
           </GridItem>
           
-           <GridItem bg = "#1C1F26" borderRadius = "20px" >
+           <GridItem bg = "#1C1F26" borderRadius = "20px" minW = "360px" >
              <Box h = "40vh">
                
              </Box>
            </GridItem>
   
-           <GridItem bg = "#1C1F26" borderRadius = "20px">
+           <GridItem bg = "#1C1F26" borderRadius = "20px" minW = "360px">
              <Box h = "40vh">
                
              </Box>
            </GridItem>
   
-           <GridItem bg = "#1C1F26" borderRadius = "20px">
+           <GridItem bg = "#1C1F26" borderRadius = "20px" minW = "360px">
              <Box h = "40vh">
                
              </Box>
