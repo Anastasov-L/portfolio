9e2c5bfc08f1dbd0ec2177c3069ef1c1ad852fc6
Lyudmil Anastasov <75137222+Anastasov-L@users.noreply.github.com>
2025-06-09 13:32:21 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
