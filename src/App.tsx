53b93079d75facef30e61c0a28a703351bdc714a
Lyudmil Anastasov <75137222+Anastasov-L@users.noreply.github.com>
2025-06-09 12:45:25 +0200

    Update App.tsx
    Colspan change on boxes

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,26 @@ function App() {
   return (
     <>
       <Box w = "100vw" bg = "#0F1115">
-        <SimpleGrid columns = {{base:1,sm:1,md: window.innerWidth <= 1100 ? 1 : 2,lg: 2}} rowGap = "25px" columnGap = "25px" w = "calc(100vw - 48px)">
-          <GridItem>
+        <SimpleGrid columns = {{base:1,sm:1,md:2,lg: 2}} rowGap = "25px" columnGap = "25px" w = "calc(100vw - 48px)">
+          <GridItem colSpan="1">
             <Box h = "18vh" borderRadius = "20px">
                <Text>Header</Text>
             </Box>
           </GridItem>
           
-           <GridItem bg = "#1C1F26" borderRadius = "20px">
+           <GridItem bg = "#1C1F26" borderRadius = "20px" colSpan="1">
              <Box h = "40vh">
                
              </Box>
            </GridItem>
   
-           <GridItem bg = "#1C1F26" borderRadius = "20px">
+           <GridItem bg = "#1C1F26" borderRadius = "20px" colSpan="1">
              <Box h = "40vh">
                
              </Box>
            </GridItem>
   
-           <GridItem bg = "#1C1F26" borderRadius = "20px">
+           <GridItem bg = "#1C1F26" borderRadius = "20px" colSpan="1">
              <Box h = "40vh">
                
              </Box>
